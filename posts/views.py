430e3e991c87b303d371a2ee453891b13f4d0c21
yuri timao <yuri-timao2011@hotmail.com>
2022-07-11 16:52:50 -0300

    Inserção do projeto no git

---
+++ posts/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from .models import Post
+from django.db.models import Q, Count, Case, When
+from comentarios.forms import FormComentario
+from comentarios.models import Comentario
+from django.contrib import messages
+
+
+class PostIndex(ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # Deixando pra apresentar os posts em ordem crescente e mostrando somente os publicados
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
+
+class PostBusca(PostIndex):
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+        return qs
+
+
+class PostCategoria(PostIndex):
+    template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)  # filtrando por categoria
+
+        return qs
+
+
+class PostDetalhes(UpdateView):
+    template_name = 'posts/post_detalhes.html'
+    model = Post
+    form_class = FormComentario
+    context_object_name = 'post'
